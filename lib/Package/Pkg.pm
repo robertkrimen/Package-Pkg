c0d2ede5e4a4ae744300c8459aad07565b13b1da
robertkrimen <robertkrimen@gmail.com>
2010-05-10 11:46:28 -0700

    Super syntactically sweet ->install

--- lib/Package/Pkg.pm
+++ lib/Package/Pkg.pm
@@ -11,15 +11,63 @@ require Sub::Install;
 sub install {
     my $self = shift;
     my %install;
-    if ( @_ == 2 ) {
-        # FIXME This does not work
-        die "This does not work";
-        @install{qw/ as into /} = ( $_[0], $_[0], $_[1] );
+    if      ( @_ == 1 ) { %install = %{ $_[0] } }
+    elsif   ( @_ == 2 ) { @install{qw/ code into /} = @_ }
+    elsif   ( @_ == 3 ) { @install{qw/ code into as /} = @_ }
+    else                { %install = @_ }
+
+    my ( $from, $code, $into, $as ) = @install{qw/ from code into as /};
+    undef %install;
+
+    die "Missing code" unless defined $code;
+
+    if ( ref $code eq 'CODE' ) {
+        die "Invalid (superfluous) from ($from) with code reference" if defined $from;
     }
     else {
-        @install{qw/ as code into /} = @_;
+        if ( defined $from )
+            { die "Invalid code ($code) with from ($from)" if $code =~ m/::/ }
+        elsif ( $code =~ m/::/)
+            { ( $from, $code ) = $self->split2( $code ) }
+        else                    
+            { $from = caller }
     }
+
+    if ( defined $as && $as =~ m/::/) {
+        die "Invalid as ($as) with into ($into)" if defined $into;
+        ( $into, $as ) = $self->split2( $as );
+    }
+    elsif ( defined $into && ! defined $as ) {
+        if ( $into =~ s/::$// ) { }
+        else {
+            ( $into, $as ) = $self->split2( $into );
+        }
+    }
+
+    if      ( defined $as ) {}
+    elsif   ( ! ref $code ) { $as = $code }
+    else                    { die "Missing as" }
+
+    die "Missing into" unless defined $into;
+
+    @install{qw/ code into as /} = ( $code, $into, $as );
+    $install{from} = $from if defined $from;
     Sub::Install::install_sub( \%install );
+}
+
+sub split {
+    my $self = shift;
+    my $target = shift;
+    return unless defined $target && length $target;
+    return split m/::/, $target;
+}
+
+sub split2 {
+    my $self = shift;
+    return unless my @split = $self->split( @_ );
+    return join '::', @split if 1 == @split;
+    my $name = pop @split;
+    return( join( '::', @split ), $name );
 }
 
 our $pkg = __PACKAGE__;
@@ -35,6 +83,14 @@ sub load {
 }
 
 sub export {
+    my $self = shift;
+    my $exporter = $self->exporter( @_ );
+
+    my $package = caller;
+    $self->install( code => $exporter, into => "${package}::import" );
+}
+
+sub exporter {
     my $self = shift;
     my ( %index, %group, $default_export );
     %group = ( default => [], optional => [], all => [] );
@@ -59,7 +115,7 @@ sub export {
     }
     $group{all} = [ map { @$_ } @group{qw/ default optional /} ];
 
-    my $import = sub {
+    my $exporter = sub {
         my ( $class ) = @_;
 
         my $package = caller;
@@ -75,12 +131,11 @@ sub export {
 
         for my $name ( @exporting ) {
             my $install = $index{$name} or die "Unrecognized export ($name)";
-            __PACKAGE__->install( @$install, $package );
+            __PACKAGE__->install( as => $install->[0], code => $install->[1], into => $package );
         }
     };
 
-    my $package = caller;
-    $self->install( import => $import => $package );
+    return $exporter;
 }
 
 1;
